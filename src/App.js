cd81b62046229ae2b500e907803e55bf9febc88a
Andrew Rehayem <andrewrehayem@gmail.com>
2018-10-29 15:11:22 -0700

    adding tracks and track previews

--- src/App.js
+++ src/App.js
@@ -116,21 +116,106 @@ class ConductorDetail extends Component {
 }
 
 class AlbumDetail extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {expanded: false};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      expanded: !state.expanded
+    }));
+  }
   render() {
     const data = this.props.data;
     return (
       <li className="album-detail">
-        <img alt="Album cover." src={data.images[0].url} />
-        <div className="album-text">
-          <h3>{data.name}</h3>
-          <div>
-            {data.artists.map((artist, index) =>
-              <p key={index}>{artist.name}</p>
-            )}
+        <div onClick={this.handleClick}>
+          <img alt="Album cover." src={data.images[0].url} />
+          <div className="album-text">
+            <h3>{data.name}</h3>
+            <div>
+              {data.artists.map((artist, index) =>
+                <p key={index}>{artist.name}</p>
+              )}
+            </div>
           </div>
         </div>
+        {this.state.expanded &&
+          <Tracks albumId={data.id} />
+        }
       </li>
     );
+  }
+}
+
+class Tracks extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {tracks: []};
+  }
+
+  componentDidMount() {
+    fetch('search/tracks?albumId=' + this.props.albumId, {mode: 'cors'})
+      .then(res => res.json())
+      .then(tracks => {
+        this.setState({
+          tracks: tracks.body.items,
+        })
+      });
+  }
+
+  render() {
+    const {tracks} = this.state;
+    return (
+      <ol className="track-list">
+        {tracks.map((track, index) =>
+          <Track key={track.id} name={track.name} preview={track.preview_url} />
+        )}
+      </ol>
+    )
+  }
+}
+
+class Track extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {expanded: false};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      expanded: !state.expanded
+    }));
+  }
+
+  render() {
+    return (
+      <li className="track-item" onClick={this.handleClick}>
+        {this.props.name}
+        {this.state.expanded &&
+          <Preview url={this.props.preview} />
+        }
+      </li>
+    )
+  }
+}
+
+class Preview extends Component {
+  render() {
+    return (
+      <audio className="audio-preview" controls>
+        <source src={this.props.url} type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio> 
+    )
   }
 }
 
